1be0d9513e275b2e7a352b0f59570822afa81554
Gautam :) <9autamchavda@gmail.com>
2026-09-14 00:45:44 +0530

    Time: 4 ms (63.17%) | Memory: 45.4 MB (88.67%) - LeetSync

--- LeetCode/76-minimum-window-substring/minimum-window-substring.java
+++ LeetCode/76-minimum-window-substring/minimum-window-substring.java
@@ -1,43 +1,49 @@
 class Solution {
     public String minWindow(String s, String t) {
+        if (s.length() == 0 || t.length() == 0) return "";
+
         int[] tCount = new int[128];
         int[] sCount = new int[128];
-        int left = 0;
 
-        int minLen = Integer.MAX_VALUE;
-        int startIdx = 0; 
-
-        for(int i = 0; i < t.length(); i++) {
+        int need = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (tCount[t.charAt(i)] == 0) {
+                need++; 
+            }
             tCount[t.charAt(i)]++;
         }
 
-        for(int right = 0; right < s.length(); right++) {
-            sCount[s.charAt(right)]++;
-            
-            while(checkSame(tCount, sCount) && left <= right) {
+        int left = 0;
+        int have = 0; 
+        int minLen = Integer.MAX_VALUE;
+        int startIdx = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char rightChar = s.charAt(right);
+            sCount[rightChar]++;
+
+            if (tCount[rightChar] > 0 && sCount[rightChar] == tCount[rightChar]) {
+                have++;
+            }
+
+            while (have == need) {
                 int currentWindowLen = right - left + 1;
                 if (currentWindowLen < minLen) {
                     minLen = currentWindowLen;
-                    startIdx = left; 
+                    startIdx = left;
+                }
+
+                char leftChar = s.charAt(left);
+                sCount[leftChar]--;
+
+                if (tCount[leftChar] > 0 && sCount[leftChar] < tCount[leftChar]) {
+                    have--;
                 }
                 
-                sCount[s.charAt(left)]--;
                 left++;
             }
         }
 
-        if (minLen == Integer.MAX_VALUE) {
-            return "";
-        }
-        return s.substring(startIdx, startIdx + minLen);
-    }
-
-    public boolean checkSame(int[] tCount, int[] sCount) {
-        for(int i = 0; i < 128; i++) {
-            if(tCount[i] != 0 && sCount[i] < tCount[i]) {
-                return false;
-            }
-        }
-        return true;
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(startIdx, startIdx + minLen);
     }
 }
