f3fc939ac79a2bb2620e73c44aff80007a47d8e0
Gautam :) <9autamchavda@gmail.com>
2026-09-13 15:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (37.75%) - LeetSync

--- LeetCode/198-house-robber/house-robber.java
+++ LeetCode/198-house-robber/house-robber.java
@@ -1,31 +1,30 @@
 class Solution {
-
-    int[][] memo = new int[100][40004];
+    int[] memo; 
 
     public int rob(int[] nums) {
-
+        memo = new int[nums.length];
         for(int i=0; i<memo.length; i++) {
-            for(int j=0; j<memo[0].length; j++) {
-                memo[i][j] = -1;
-            }
-        }
-        return solve(nums, 0, 0);
+            memo[i] = -1;
+        } 
+        
+        return solve(nums, 0);
     }
 
-    public int solve(int[] nums, int idx, int count) {
-        if(idx >= nums.length) {
-            return count;
+    public int solve(int[] nums, int idx) {
+        if (idx >= nums.length) {
+            return 0;
         }
 
-        if(memo[idx][count] != -1) {
-            return memo[idx][count];
+        if (memo[idx] != -1) {
+            return memo[idx];
         }
 
-        int skip = solve(nums, idx+1, count);
-        int rob = solve(nums, idx+2, count+nums[idx]);
+        int skip = solve(nums, idx + 1);
 
-        memo[idx][count] = Math.max(skip, rob);
+        int rob = nums[idx] + solve(nums, idx + 2);
+
+        memo[idx] = Math.max(skip, rob);
         
-        return memo[idx][count];
+        return memo[idx];
     }
 }
