889ded579a124598f6ebefc2ae2d823865b16b65
Gautam :) <9autamchavda@gmail.com>
2026-09-14 16:58:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (52.92%) - LeetSync

--- LeetCode/866-rectangle-overlap/rectangle-overlap.java
+++ LeetCode/866-rectangle-overlap/rectangle-overlap.java
@@ -1,10 +1,6 @@
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
         
-        if (rec1[0] == rec1[2] || rec1[1] == rec1[3] || rec2[0] == rec2[2] || rec2[1] == rec2[3]) {
-            return false;
-        }
-        
         boolean xOverlap = rec1[0] < rec2[2] && rec2[0] < rec1[2];
         boolean yOverlap = rec1[1] < rec2[3] && rec2[1] < rec1[3];
         
